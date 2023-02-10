e99fd00960a9176576924cb01823752725627052
Igor Franca <igor@LAPTOP-2K4O0V7E.localdomain>
2023-02-10 17:28:07 -0300

    LPS-175304 Create Licensing price screen

---
+++ src/pages/InformLicensingTermsPage/InformLicensingTermsPricePage.tsx
@@ -0,0 +1,41 @@
+import { Header } from "../../components/Header/Header";
+import { Section } from "../../components/Section/Section";
+
+import "./InformLicensingTermsPage.scss";
+import { NewAppPageFooterButtons } from "../../components/NewAppPageFooterButtons/NewAppPageFooterButtons";
+import { LicensePriceCard } from "../../components/LicensePriceCard/LicensePriceCard";
+import React from "react";
+
+interface InformLicensingTermsPricePageProps {
+  onClickBack: () => void;
+  onClickContinue: () => void;
+}
+
+export function InformLicensingTermsPricePagePage({
+  onClickBack,
+  onClickContinue,
+}: InformLicensingTermsPricePageProps) {
+  return (
+    <div className="informing-licensing-terms-page-container">
+      <Header
+        description="Define the licensing approach for your app. This will impact users' licensing renew experience."
+        title="Inform licensing terms"
+      />
+
+      <Section
+        label="Standard License prices"
+        required
+        tooltip="More Info"
+        tooltipText="More Info"
+      >
+        <LicensePriceCard price={""} currency={""} />
+      </Section>
+
+      <NewAppPageFooterButtons
+        onClickBack={() => onClickBack()}
+        onClickContinue={() => onClickContinue()}
+        showBackButton
+      />
+    </div>
+  );
+}
